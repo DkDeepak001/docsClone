bc2e708b022401e6178797e585dd046268187d63
DkDeepak001 <Deepakeswar001@gmail.com>
2023-08-19 11:08:38 +0530

    feat: socket config

--- src/pages/docs/[id].tsx
+++ src/pages/docs/[id].tsx
@@ -1,5 +1,7 @@
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
 
 const DynamicEditor = dynamic(() => import("~/components/editor"), {
   loading: () => <p>Loading...</p>,
@@ -8,6 +10,16 @@ const DynamicEditor = dynamic(() => import("~/components/editor"), {
 
 export default function Docs() {
   const { id } = useRouter().query;
+
+  useEffect(() => {
+    const socket = io("http://localhost:8000");
+    socket.emit("join-room", id);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <div className="max-h-full min-h-screen bg-gray-50">
       <DynamicEditor />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useState } from "react";
 import { api } from "~/utils/api";
 
 export default function Home() {
