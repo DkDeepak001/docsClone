70357b3f0fe6f7bd3b31db2adeec05cf10f29189
DkDeepak001 <Deepakeswar001@gmail.com>
2024-04-16 18:34:37 +0530

    feat: add icons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,13 @@
 import { ModeToggle } from "./ui/themeToggle"
-
+import Logo from '../../public/icon.png'
+import Image from "next/image"
 export const Header = () => {
   return (
-    <div className="w-full h-20  ">
+    <div className="w-full h-20 flex flex-row items-center justify-between border-b-[1px] border-b-foreground/20 px-20">
+      <div className="flex flex-row items-center justify-center gap-x-2">
+        <Image src={Logo} alt="logo" className="h-12 w-12" />
+        <h2 className="text-foreground font-black text-xl">Docs</h2>
+      </div>
       <ModeToggle />
     </div>
   )
